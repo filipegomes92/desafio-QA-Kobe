93430ffe86c9a684fc2e0ac068d83caf46969487
Filipe Soares Gomes <filipe.92.gomes>
2025-08-15 01:28:12 -0300

    commit projeto e videos desafio kobe

---
+++ Tests/Elementos/Login.js
@@ -0,0 +1,10 @@
+// Tela de login.js
+output.telalogin = {
+    botaoConta: "account",
+    botaoEntrarEmail: "Entrar com e-mail",
+    botaoEmailESenha: "Entrar com e-mail e senha",
+    assertTelaLogin: "Entrar com e-mail",
+    campoEmail: "E-mail",
+    campoSenha: "Senha",
+    botaoEntrarLogin: "Entrar"
+}
